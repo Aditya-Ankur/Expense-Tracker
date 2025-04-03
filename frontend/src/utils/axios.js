3d341f8770beeccd323a63514462564763e445c6
Aditya <66164756+Aditya-Ankur@users.noreply.github.com>
2025-04-04 02:26:27 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',  // baseURL is the base URL for all requests which will be sent using this instance
+    baseURL: 'https://expense-tracker-41xz.onrender.com',  // baseURL is the base URL for all requests which will be sent using this instance
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
